fdae8b11639252c3dbff9fb5b98b1af74f611d0f
Emads <emadsaab722@gmail.com>
2024-08-08 21:13:57 +0300

    complete implementation of doubly linked list data structure

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -6,11 +6,12 @@ ItemType = TypeVar('ItemType')
 
 class Node(Generic[ItemType]):
     """
-    A class representing a node in a linked list.
+    A class representing a node in a doubly linked list.
 
     Attributes:
         value (ItemType): The value stored in the node.
-        next (Optional[Node[ItemType]]): The reference to the next node in the linked list.
+        next (Optional[Node[ItemType]]): The reference to the next node in the list.
+        prev (Optional[Node[ItemType]]): The reference to the previous node in the list.
     """
 
     def __init__(self, value: ItemType) -> None:
@@ -22,6 +23,7 @@ class Node(Generic[ItemType]):
         """
         self.value = value
         self.next: Optional[Node[ItemType]] = None
+        self.prev: Optional[Node[ItemType]] = None
 
     def __repr__(self) -> str:
         """
@@ -33,19 +35,19 @@ class Node(Generic[ItemType]):
         return f"Node({self.value})"
 
 
-class LinkedList(Generic[ItemType]):
+class DoublyLinkedList(Generic[ItemType]):
     """
-    A class representing a singly linked list.
+    A class representing a doubly linked list.
 
     Attributes:
-        head (Optional[Node[ItemType]]): The head node of the linked list.
-        tail (Optional[Node[ItemType]]): The tail node of the linked list.
-        length (int): The number of nodes in the linked list.
+        head (Optional[Node[ItemType]]): The head node of the list.
+        tail (Optional[Node[ItemType]]): The tail node of the list.
+        length (int): The number of nodes in the list.
     """
 
     def __init__(self) -> None:
         """
-        Initialize an empty linked list.
+        Initialize an empty doubly linked list.
         """
         self.head: Optional[Node[ItemType]] = None
         self.tail: Optional[Node[ItemType]] = None
@@ -53,15 +55,17 @@ class LinkedList(Generic[ItemType]):
 
     def __repr__(self) -> str:
         """
-        Return a string representation of the linked list.
+        Return a string representation of the doubly linked list.
 
         Returns:
-            str: The string representation of the linked list.
+            str: The string representation of the doubly linked list.
         """
-        display = "* "
+        first_link = "None <-- " if self.length > 0 else ""
+        display = f"* {first_link}"
         temp = self.head
         while temp:
-            display += f"{temp} --> "
+            link = "<-->" if temp.next else "-->"
+            display += f"{temp} {link} "
             temp = temp.next
         display += f"None\n\n  . Head: {self.head}\n  . Tail: {
             self.tail}\n  . Length: {self.length}\n\n"
@@ -69,31 +73,29 @@ class LinkedList(Generic[ItemType]):
 
     def display(self) -> None:
         """
-        Print the string representation of the linked list.
+        Print the string representation of the doubly linked list.
         """
         print(self)
 
     def empty(self) -> bool:
         """
-        Check if the linked list is empty.
+        Check if the doubly linked list is empty.
 
         Returns:
-            bool: True if the linked list is empty, False otherwise.
+            bool: True if the list is empty, False otherwise.
         """
         return self.length == 0
 
     def clear(self) -> None:
         """
-        Clear the linked list by resetting head, tail, and length attributes.
-
-        This method removes all nodes from the linked list, effectively making it an empty list.
+        Clear the doubly linked list by resetting head, tail, and length attributes.
         """
         self.head = self.tail = None
         self.length = 0
 
     def append(self, value: ItemType) -> bool:
         """
-        Append a new node with a given value to the end of the linked list.
+        Append a new node with a given value to the end of the list.
 
         Args:
             value (ItemType): The value to be stored in the new node.
@@ -103,18 +105,17 @@ class LinkedList(Generic[ItemType]):
         """
         new_node = Node(value)
         if self.empty():
-            # If the list is empty, set the new node as both head and tail
             self.head = self.tail = new_node
         else:
-            # Append the new node to the end of the list
-            self.tail.next = new_node  # self.tail is guaranteed to be not None here
+            self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
         return True
 
     def prepend(self, value: ItemType) -> bool:
         """
-        Prepend a new node with a given value to the start of the linked list.
+        Prepend a new node with a given value to the start of the list.
 
         Args:
             value (ItemType): The value to be stored in the new node.
@@ -124,10 +125,9 @@ class LinkedList(Generic[ItemType]):
         """
         new_node = Node(value)
         if self.empty():
-            # If the list is empty, set the new node as both head and tail
             self.head = self.tail = new_node
         else:
-            # Insert the new node at the start of the list
+            self.head.prev = new_node
             new_node.next = self.head
             self.head = new_node
         self.length += 1
@@ -135,29 +135,26 @@ class LinkedList(Generic[ItemType]):
 
     def pop(self) -> Optional[Node[ItemType]]:
         """
-        Remove and return the last node from the linked list.
+        Remove and return the last node from the list.
 
         Returns:
             Optional[Node[ItemType]]: The removed node, or None if the list is empty.
         """
         if self.empty():
             return None
-        node_before = node_to_pop = self.head
-        while node_to_pop.next:
-            node_before = node_to_pop
-            node_to_pop = node_to_pop.next
-        # Disconnect the last node from the list
-        node_before.next = None
-        self.tail = node_before
-        self.length -= 1
-        if self.empty():
-            # If the list becomes empty, reset head and tail to None
+        node_to_pop = self.tail
+        if self.length == 1:
             self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            node_to_pop.prev = None
+        self.length -= 1
         return node_to_pop
 
     def pop_first(self) -> Optional[Node[ItemType]]:
         """
-        Remove and return the first node from the linked list.
+        Remove and return the first node from the list.
 
         Returns:
             Optional[Node[ItemType]]: The removed node, or None if the list is empty.
@@ -166,18 +163,17 @@ class LinkedList(Generic[ItemType]):
             return None
         node_to_pop = self.head
         if self.length == 1:
-            # If there is only one node in the list, reset head and tail to None
             self.head = self.tail = None
         else:
-            # Move head to the next node
             self.head = self.head.next
-        node_to_pop.next = None  # Disconnect the popped node from the list
+            self.head.prev = None
+            node_to_pop.next = None
         self.length -= 1
         return node_to_pop
 
     def get(self, index: int) -> Optional[Node[ItemType]]:
         """
-        Retrieve the node at a specific index in the linked list.
+        Retrieve the node at a specific index in the list.
 
         Args:
             index (int): The index of the node to retrieve.
@@ -187,14 +183,19 @@ class LinkedList(Generic[ItemType]):
         """
         if index < 0 or index >= self.length:
             return None
-        node_to_get = self.head
-        for _ in range(index):
-            node_to_get = node_to_get.next
+        if index < self.length / 2:
+            node_to_get = self.head
+            for _ in range(index):
+                node_to_get = node_to_get.next
+        else:
+            node_to_get = self.tail
+            for _ in range(self.length - 1, index, -1):
+                node_to_get = node_to_get.prev
         return node_to_get
 
     def set_value(self, index: int, value: ItemType) -> bool:
         """
-        Set the value of the node at a specific index in the linked list.
+        Set the value of the node at a specific index in the list.
 
         Args:
             index (int): The index of the node to update.
@@ -211,7 +212,7 @@ class LinkedList(Generic[ItemType]):
 
     def insert(self, index: int, value: ItemType) -> bool:
         """
-        Insert a new node with a given value at a specific index in the linked list.
+        Insert a new node with a given value at a specific index in the list.
 
         Args:
             index (int): The index at which to insert the new node.
@@ -226,16 +227,21 @@ class LinkedList(Generic[ItemType]):
             return self.prepend(value)
         if index == self.length:
             return self.append(value)
+
         new_node = Node(value)
         node_before = self.get(index - 1)
-        new_node.next = node_before.next
+        node_after = node_before.next
+
+        new_node.prev = node_before
+        new_node.next = node_after
         node_before.next = new_node
+        node_after.prev = new_node
         self.length += 1
         return True
 
     def remove(self, index: int) -> Optional[Node[ItemType]]:
         """
-        Remove and return the node at a specific index in the linked list.
+        Remove and return the node at a specific index in the list.
 
         Args:
             index (int): The index of the node to remove.
@@ -249,33 +255,10 @@ class LinkedList(Generic[ItemType]):
             return self.pop_first()
         if index == self.length - 1:
             return self.pop()
-        node_before = self.get(index - 1)
-        node_to_remove = node_before.next
-        node_before.next = node_to_remove.next
-        node_to_remove.next = None
+
+        node_to_pop = self.get(index)
+        node_to_pop.prev.next = node_to_pop.next
+        node_to_pop.next.prev = node_to_pop.prev
+        node_to_pop.prev = node_to_pop.next = None
         self.length -= 1
-        return node_to_remove
-
-    def reverse(self) -> None:
-        """
-        Reverse the linked list in place.
-
-        This method reverses the order of the nodes in the linked list.
-        After reversal, the head becomes the tail and vice versa.
-
-        Returns:
-            None
-        """
-        current = self.head
-        # Swap the head and tail
-        self.head, self.tail = self.tail, self.head
-        before = after = None
-
-        while current:
-            # Temporarily store the next node
-            after = current.next
-            # Reverse the current node's pointer
-            current.next = before
-            # Move pointers one position ahead
-            before = current
-            current = after
+        return node_to_pop
