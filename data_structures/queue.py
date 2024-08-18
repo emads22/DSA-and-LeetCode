48a91a4eaf084cb220aa99cc724c74072d13df11
Emads <emadsaab722@gmail.com>
2024-08-18 12:02:17 +0300

    complete implementation of stack and queue data structures

---
+++ data_structures/queue.py
@@ -0,0 +1,141 @@
+from typing import TypeVar, Generic, Optional
+
+
+ItemType = TypeVar('ItemType')
+
+
+class Node(Generic[ItemType]):
+    """
+    A class representing a node in a queue represented as a linked list.
+
+    Attributes:
+        value (ItemType): The value stored in the node.
+        next (Optional[Node[ItemType]]): The reference to the next node in the linked list.
+    """
+
+    def __init__(self, value: ItemType) -> None:
+        """
+        Initialize a new node with a given value.
+
+        Args:
+            value (ItemType): The value to be stored in the node.
+        """
+        self.value = value
+        self.next: Optional[Node[ItemType]] = None
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the node.
+
+        Returns:
+            str: The string representation of the node.
+        """
+        return f"({self.value})"
+
+
+class Queue(Generic[ItemType]):
+
+    """
+    A class representing a queue.
+
+    Attributes:
+        first (Optional[Node[ItemType]]): The first node of the queue.
+        last (Optional[Node[ItemType]]): The last node of the queue.
+        length (int): The number of nodes in the queue.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize an empty queue represented as a linked list.
+        """
+        self.first: Optional[Node[ItemType]] = None
+        self.last: Optional[Node[ItemType]] = None
+        self.length: int = 0
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the queue.
+
+        Returns:
+            str: The string representation of the queue.
+        """
+        display = "\n\n*         <-- IN"
+        runner = ""
+        temp = self.first
+        while temp:
+            runner = f"\n  | {temp} |" + runner
+            temp = temp.next
+        else:
+            if runner == "":
+                runner = "\n  |     |\n  |     |"
+        display += runner + f"\n          --> OUT\n\n  . First: {self.first}\n  . Last: {self.last}\n  . Length: {self.length}\n"
+        return display
+
+    def display(self) -> None:
+        """
+        Print the string representation of the queue.
+        """
+        print(self)
+
+    def empty(self) -> bool:
+        """
+        Check if the queue is empty.
+
+        Returns:
+            bool: True if the queue is empty, False otherwise.
+        """
+        return self.length == 0
+
+    def clear(self) -> None:
+        """
+        Clear the queue by resetting first, last, and length attributes.
+
+        This method removes all nodes from the queue (linked list), effectively making it an empty queue.
+        """
+        self.first = self.last = None
+        self.length = 0
+
+    def enqueue(self, value: ItemType) -> bool:
+        """
+        Add a new value to the end of the queue.
+
+        Args:
+            value (ItemType): The value to be added to the queue.
+
+        Returns:
+            bool: True indicating the value was successfully added to the queue.
+        """
+        new_node = Node(value)
+        if self.empty():
+            # If the queue is empty, the new node becomes both the first and last node.
+            self.first = self.last = new_node
+        else:
+            # Link the last node's next reference to the new node and update the last node.
+            self.last.next = new_node
+            self.last = new_node
+        # Increment the length of the queue as a new node has been added.
+        self.length += 1
+        return True
+
+    def dequeue(self) -> Optional[Node[ItemType]]:
+        """
+        Remove and return the value from the front of the queue.
+
+        Returns:
+            Optional[Node[ItemType]]: The node that was dequeued from the front of the queue, or None if the queue is empty.
+        """
+        if self.empty():
+            return None  # if the queue is empty.
+        # Store the first node to return after removing it from the queue.
+        node_to_dequeue = self.first
+        if self.length == 1:
+            # If the queue has only one node, reset both first and last references to None.
+            self.first = self.last = None
+        else:
+            # Update the first reference to the next node in the queue.
+            self.first = self.first.next
+            # Detach the dequeued node from the queue.
+            node_to_dequeue.next = None
+        # Decrement the length of the queue as a node has been removed.
+        self.length -= 1
+        return node_to_dequeue
---
+++ data_structures/stack.py
@@ -0,0 +1,136 @@
+from typing import TypeVar, Generic, Optional
+
+
+ItemType = TypeVar('ItemType')
+
+
+class Node(Generic[ItemType]):
+    """
+    A class representing a node in a stack represented as a linked list.
+
+    Attributes:
+        value (ItemType): The value stored in the node.
+        next (Optional[Node[ItemType]]): The reference to the next node in the linked list.
+    """
+
+    def __init__(self, value: ItemType) -> None:
+        """
+        Initialize a new node with a given value.
+
+        Args:
+            value (ItemType): The value to be stored in the node.
+        """
+        self.value = value
+        self.next: Optional[Node[ItemType]] = None
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the node.
+
+        Returns:
+            str: The string representation of the node.
+        """
+        return f"({self.value})"
+
+
+class Stack(Generic[ItemType]):
+
+    """
+    A class representing a stack.
+
+    Attributes:
+        top (Optional[Node[ItemType]]): The head node of the stack.
+        height (int): The number of nodes in the stack.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize an empty stack represented as a linked list.
+        """
+        self.top: Optional[Node[ItemType]] = None
+        self.height: int = 0
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the stack.
+
+        Returns:
+            str: The string representation of the stack.
+        """
+        display = "\n\n*         <-- IN\n          --> OUT"
+        runner = ""
+        temp = self.top
+        while temp:
+            runner += f"\n  | {temp} |"
+            temp = temp.next
+        else:
+            if runner == "":
+                runner += f"\n  |     |"
+        display += runner + \
+            f"\n  |_____|\n\n  . Top: {self.top}\n  . Height: {self.height}\n"
+        return display
+
+    def display(self) -> None:
+        """
+        Print the string representation of the stack.
+        """
+        print(self)
+
+    def empty(self) -> bool:
+        """
+        Check if the stack is empty.
+
+        Returns:
+            bool: True if the stack is empty, False otherwise.
+        """
+        return self.height == 0
+
+    def clear(self) -> None:
+        """
+        Clear the stack by resetting top and height attributes.
+
+        This method removes all nodes from the stack (linked list), effectively making it an empty stack.
+        """
+        self.top = None
+        self.height = 0
+
+    def push(self, value: ItemType) -> bool:
+        """
+        Push a new value onto the stack.
+
+        Args:
+            value (ItemType): The value to be added to the stack.
+
+        Returns:
+            bool: True indicating the value was successfully pushed onto the stack.
+        """
+        new_node = Node(value)
+        if self.empty():
+            # If the stack is empty, the new node becomes the top node.
+            self.top = new_node
+        else:
+            # Link the new node to the current top node and update the top.
+            new_node.next = self.top
+            self.top = new_node
+        # Increment the height of the stack as a new node has been added.
+        self.height += 1
+        return True
+
+    def pop(self) -> Optional[Node[ItemType]]:
+        """
+        Pop the top value from the stack.
+
+        Returns:
+            Optional[Node[ItemType]]: The node that was popped from the stack, or None if the stack is empty.
+        """
+        if self.empty():
+            return None  # if the stack is empty.
+        # Store the top node to return after removing it from the stack.
+        node_to_pop = self.top
+        # Update the top to the next node in the stack.
+        self.top = self.top.next
+        # Detach the popped node from the stack.
+        node_to_pop.next = None
+        # Decrement the height of the stack as a node has been removed.
+        self.height -= 1
+        return node_to_pop
